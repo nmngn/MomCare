e6d5438a46bdaf2ebeef4a0aef4e5fdb3159f883
Nam Ngây <>
2021-12-20 21:25:26 +0700

    Search VC

---
+++ MomCare/Controller/SearchVC/Cell/SearchItemTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  SearchItemTableViewCell.swift
+//  MomCare
+//
+//  Created by Nam Ngây on 20/12/2021.
+//
+
+import UIKit
+
+class SearchItemTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var subView: UIView!
+    @IBOutlet weak var avatarUser: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var dayCreateLabel: UILabel!
+    @IBOutlet weak var babyAgeLabel: UILabel!
+    @IBOutlet weak var dateBornLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        subView.makeShadow()
+        subView.makeBorderColor()
+    }
+    
+    func setupData(model: HomeModel) {
+        DispatchQueue.main.async {
+            self.avatarUser.image = model.loadImageFromDiskWith(fileName: model.numberPhone) ?? UIImage(named: "avatar_placeholder")
+        }
+        userNameLabel.text = model.name
+        dayCreateLabel.text = model.dateSave
+        babyAgeLabel.text = model.dateCalculate
+        dateBornLabel.text = model.babyAge
+    }
+
+}
---
+++ MomCare/Controller/SearchVC/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  MomCare
+//
+//  Created by Nam Ngây on 20/12/2021.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
