6ab03800451538cfb1b38da0f6ebeaff3bfc6702
Nguyễn Trung Nam <>
2022-06-17 16:50:07 +0700

    SettingVC

---
+++ MomCare/Controller/SettingVC/SettingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingViewController.swift
+//  MomCare
+//
+//  Created by Nam Ngây on 17/06/2022.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
